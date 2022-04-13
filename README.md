738ad26850d3d5aed5f798e9adbffa4357c804b1
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-04-13 14:44:17 -0700

    Added Mouse Movement and Clicking Functionality
    Using the mouse is now another option on top of arrow keys for moving the player ship!

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rocket-Patrol-Mod
+Mods Included:
+- Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+- Implement mouse control for player movement and mouse click to fire (20)
+- Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+
---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,51 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.isFiring = false;
+      this.moveSpeed = 2;
+
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+
+        if (!this.isFiring && 
+            game.input.mousePointer.distance > 0 &&
+            game.input.mousePointer.x >= borderUISize + this.width && 
+            game.input.mousePointer.x <= game.config.width - borderUISize - this.width) {
+            this.x = game.input.mousePointer.x;
+        }
+
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) || game.input.mousePointer.leftButtonDown() && 
+        !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+  }
