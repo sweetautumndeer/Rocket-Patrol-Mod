e3444c98be1d3072399f47c1b2a1b40279be360d
silmist <70921116+silmist@users.noreply.github.com>
2022-04-28 15:55:18 -0700

    Revert "commit on 'Main'"
    This reverts commit e9347b3b3ee4e4dc5c65f50629ed03b5502f41cf.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -20,7 +20,7 @@ class Menu extends Phaser.Scene {
     create() {
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Georgia',
+            fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#AA11AA',
             color: '#FF77FF',
